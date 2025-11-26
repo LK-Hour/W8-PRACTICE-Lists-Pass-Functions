90d51b0077ddb50643c67044601d4aa1110c931d
LK-Hour <l.kimhour.609@gmail.com>
2025-11-27 00:41:39 +0700

    EX3: Update exercise 3

--- lib/EXERCISE-3/main.dart
+++ lib/EXERCISE-3/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'data/profile_data.dart';
 import 'ui/screens/profile.dart';
- 
+
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: ProfileApp(profileData: ronanProfile),
-  ));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ProfileApp(profileData: ronanProfile),
+    ),
+  );
 }
